5afd22864f24072d5b0cfbcb5c87c09271054649
BonalihengOun <bonalyhengun@gmail.com>
2024-04-11 11:12:45 +0700

    image default

--- src/app/components/MovieCardComponents.jsx
+++ src/app/components/MovieCardComponents.jsx
@@ -1,41 +1,51 @@
 import { Carousel, CarouselContent } from "@/components/ui/carousel";
 import { listMoviewithGenre } from "@/service/movieService";
 import Link from "next/link";
-const CardComponents = async ({gen}) => {
-
+const CardComponents = async ({ gen }) => {
   const getCategoryMovie = await listMoviewithGenre(gen);
 
   return (
     <div className="">
       <div className="flex overflow-x-auto px-7 bg-[#612114]">
-       <Carousel className="w-full">
-        <CarouselContent>
-        {getCategoryMovie.payload?.map((movie) => (
-        <div className="carousel px-5 gap-5 overflow-visible">
-          
-            <Link href={`/view-movie-details/${movie.movie_id}`}>
-              <div
-                key={movie.movie_id}
-                className="carousel-item card w-96 bg-white shadow-xl"
-              >
-                <figure className="px-7 pt-7">
-                  <img src={movie.image} alt="Shoes" className="rounded-xl" />
-                </figure>
-                <div className="card-body  text-center">
-                  <h2 className="card-title line-clamp-1 text-start text-black">
-                    {movie.movie_title}
-                  </h2>
-                  <p className="line-clamp-2 text-start text-gray-800">
-                    {movie.description}
-                  </p>
-                </div>
+        <Carousel className="w-full">
+          <CarouselContent>
+            {getCategoryMovie.payload?.map((movie) => (
+              <div className="carousel px-5 gap-5 overflow-visible">
+                <Link href={`/view-movie-details/${movie.movie_id}`}>
+                  <div
+                    key={movie.movie_id}
+                    className="carousel-item card w-96 bg-white shadow-xl"
+                  >
+                    <figure className="px-7 pt-7">
+                      {movie.image && movie.image.trim() !== "" ? (
+                        <img
+                          src={movie.image}
+                          alt={movie.movie_title || "Default description"}
+                          className="rounded-xl"
+                        />
+                      ) : (
+                        <img
+                          src="/src/asset/DSC00589.jpg"
+                          alt="Image not found"
+                          className="rounded-xl"
+                        />
+                      )}
+                    </figure>
+
+                    <div className="card-body  text-center">
+                      <h2 className="card-title line-clamp-1 text-start text-black">
+                        {movie.movie_title}
+                      </h2>
+                      <p className="line-clamp-2 text-start text-gray-800">
+                        {movie.description}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          
-        </div>
-        ))}
-        </CarouselContent>
-       </Carousel>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </div>
     </div>
   );
